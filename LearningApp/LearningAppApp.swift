7a49879f2ae2b9fc6cb7fbd546892472a01eda2a
Lucky Osunbiyi <osunbiyi.temitope1@gmail.com>
2021-06-30 18:02:55 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Lucky Osunbiyi on 2021-06-30.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
